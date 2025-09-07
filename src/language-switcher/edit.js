a8c756a7a8ffc77a3c16fe82aba381dadf8c1876
Simon Meulenbeek <simonmeulenbeek@gmail.com>
2025-09-07 17:04:27 +0200

    remove unnecessary import comments

--- src/language-switcher/edit.js
+++ src/language-switcher/edit.js
@@ -1,16 +1,4 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import {
 	PanelBody,
@@ -19,12 +7,7 @@ import {
 	RadioControl,
 } from '@wordpress/components';
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
+/** Import editor.scss so it's get processed by webpack */
 import './editor.scss';
 
 const languageSwitcherData = window.SMBLanguageSwitcherData || {};
